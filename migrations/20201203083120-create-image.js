77c629ade2a970124fba5d0e9d981db23bbda473
Tatiana Ivanova <ivanova.tatiana.w@gmail.com>
2020-12-03 12:50:03 +0100

    +userId column

--- migrations/20201203083120-create-image.js
+++ migrations/20201203083120-create-image.js
@@ -15,6 +15,9 @@ module.exports = {
       url: {
         type: Sequelize.STRING,
       },
+      userId: {
+        type: Sequelize.INTEGER,
+      },
       createdAt: {
         allowNull: false,
         type: Sequelize.DATE,
--- models/image.js
+++ models/image.js
@@ -15,6 +15,7 @@ module.exports = (sequelize, DataTypes) => {
     {
       title: { type: DataTypes.STRING, allowNull: false },
       url: DataTypes.STRING,
+      userId: DataTypes.STRING,
     },
     {
       sequelize,
