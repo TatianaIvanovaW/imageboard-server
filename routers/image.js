d303cad5b36d9133e7864ff51a77d8bbf10dd06c
Tatiana Ivanova <ivanova.tatiana.w@gmail.com>
2020-12-03 13:19:50 +0100

    add two more routers

--- routers/image.js
+++ routers/image.js
@@ -5,8 +5,33 @@ const Image = require("../models").image;
 const router = new Router();
 
 router.get("/", async (req, res) => {
-  const images = await Image.findAll();
-  res.status(200).send(images);
+  try {
+    const images = await Image.findAll();
+    res.status(200).send(images);
+  } catch (e) {
+    next(e);
+  }
+});
+
+router.post("/", async (req, res) => {
+  try {
+    const newImage = await Image.create(req.body);
+    res.status(200).json(newImage);
+  } catch (e) {
+    next(e);
+  }
+});
+
+router.get("/:id", async (req, res) => {
+  const imageId = req.params.id;
+
+  try {
+    const imageById = await Image.findByPk(imageId);
+    if (!imageById) return res.status(404).send("image not found");
+    res.status(200).send(imageById);
+  } catch (e) {
+    next(e);
+  }
 });
 
 module.exports = router;
--- routers/user.js
+++ routers/user.js
@@ -9,4 +9,13 @@ router.get("/", async (req, res) => {
   res.status(200).send(users);
 });
 
+router.post("/", async (req, res) => {
+  try {
+    const newUser = await User.create(req.body);
+    res.status(200).json(newUser);
+  } catch (e) {
+    next(e);
+  }
+});
+
 module.exports = router;
